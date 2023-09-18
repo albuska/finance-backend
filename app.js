576bf256790ddb652777feacdcaeafd2a74df23c
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-09-18 21:41:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,18 +13,7 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'false');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).json(({ body: 'OK' }));
-  }
-
-  return next();
-}));
+app.use(cors({origin: "*",}));
 
 app.use(express.json());
 
