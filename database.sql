9258912e860e41b877c6b681e751bc0a5d8ac552
Albina Anistratenko <albuska2013@gmail.com>
2023-09-26 14:18:02 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users(
     id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     token VARCHAR(255),
     balance NUMERIC(10, 2),
     start_balance NUMERIC(10, 2),
