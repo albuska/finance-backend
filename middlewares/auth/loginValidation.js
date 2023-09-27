977b93df4d4551b1ac1d9b181fa1a0e90574b2cf
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-09-27 12:58:25 +0300

    add: swagger auth routes

--- middlewares/auth/loginValidation.js
+++ middlewares/auth/loginValidation.js
@@ -9,7 +9,7 @@ const loginValidation = async (req, res, next) => {
     if (error) next(httpError(400, error.message));
 
     const { rows: user } = await db.query(`
-        SELECT id, name, email, password 
+        SELECT id, name, email, password, is_verified 
         FROM users 
         WHERE email=$1`,
         [value.email]
@@ -17,11 +17,13 @@ const loginValidation = async (req, res, next) => {
 
     if (user.length === 0) next(httpError(401, 'Email or password is wrong'));
 
-    const {password: dbPassword} = user[0]
+    const {password: dbPassword, is_verified} = user[0]
 
     const comparedPassword = await bcrypt.compare(req.body.password, dbPassword);
 
     if (!comparedPassword) next(httpError(401, 'Email or password is wrong'));
+
+    if (!is_verified) next(httpError(401, "Email not verified"));
 
     req.user = user[0];
 
