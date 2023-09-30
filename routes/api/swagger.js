b1d09a26bc2b21037269cc3ae2b4c19d651e87b7
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-09-30 21:02:59 +0300

    Update swagger.js

--- routes/api/swagger.js
+++ routes/api/swagger.js
@@ -6,6 +6,7 @@ const swaggerRouter = express.Router();
 
 const { BASE_URL, FRONT_DEV} = process.env;
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const options = {
     // requestInterceptor: function(request){
@@ -16,7 +17,7 @@ const options = {
     url: BASE_URL,
     explorer: true,
     validatorUrl: null,
-    CSS_URL: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+    customCssUrl: CSS_URL
 };
 
 swaggerRouter.use('/', swaggerUi.serve);
