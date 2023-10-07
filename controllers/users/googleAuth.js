14ba989cf2119d01d8b24e678968e7fc7d938506
Albina Anistratenko <albuska2013@gmail.com>
2023-10-07 12:36:09 +0300

    Update googleAuth.js

--- controllers/users/googleAuth.js
+++ controllers/users/googleAuth.js
@@ -5,7 +5,7 @@ exports.googleAuth = catchAsync(async (req, res) => {
   const { id } = req.user;
   const {token, refreshToken} = await getToken(id);
 
-await db.query('UPDATE users SET token=$1, refreshToken=$2 WHERE id=$2', [token, refreshToken, id]);
+await db.query('UPDATE users SET token=$1, refresh_token=$2 WHERE id=$2', [token, refreshToken, id]);
 
   res.redirect(`https://nmarkhotsky.github.io/finance-front/finance-front/?token=${token}`);
 });
