31ae8aa1374a7d9152d104c7d09f28886e221f6a
Albina Anistratenko <albuska2013@gmail.com>
2023-10-07 23:27:38 +0300

    correct

--- controllers/users/googleAuth.js
+++ controllers/users/googleAuth.js
@@ -7,11 +7,14 @@ exports.googleAuth = catchAsync(async (req, res) => {
   const { id } = req.user;
   const { token, refreshToken } = await getToken(id);
 
-  // await db.query("UPDATE users SET token=$1, refresh_token=$2 WHERE id=$3", [
-  //   token,
-  //   refreshToken,
-  //   id,
-  // ]);
+  let URL = process.env.NODE_ENV === "development" ? FRONTEND_URL :  FRONT_PROD;
+
+
+  await db.query("UPDATE users SET token=$1, refresh_token=$2 WHERE id=$3", [
+    token,
+    refreshToken,
+    id,
+  ]);
 
   // res.cookie("refreshToken", refreshToken, {
   //   maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,7 +22,7 @@ exports.googleAuth = catchAsync(async (req, res) => {
   //   secure: true,
   // });
 
-  res.redirect(`https://nmarkhotsky.github.io/finance-front?token=${token}`);
+  res.redirect(`${URL}?token=${token}`);
 
   // res.redirect(`${FRONT_PROD}?token=${token}`);
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { PORT } = process.env;
 
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
 const app = require("./app");
 
 
