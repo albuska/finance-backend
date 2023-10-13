fe18a3dbc1a6dd751e316c707a2358be8e687a82
Albina Anistratenko <albuska2013@gmail.com>
2023-10-13 08:45:34 +0300

    Update googleAuthenticate.js

--- middlewares/auth/googleAuthenticate.js
+++ middlewares/auth/googleAuthenticate.js
@@ -26,38 +26,55 @@ const googleCallback = async (
 ) => {
   const account = profile._json;
   try {
+    // Перевірка наявності користувача за google_id
     const existingUserQuery = await db.query('SELECT * FROM users WHERE google_id=$1', [account.sub]);
 
     if (existingUserQuery.rows.length > 0) {
-      
-      const existingEmailQuery = await db.query('SELECT * FROM users WHERE email=$1', [account.email]);
+      const existingUser = existingUserQuery.rows[0];
 
-      if (existingEmailQuery.rows.length > 0) {
-        done(null, existingUserQuery.rows[0]);
-        // return done(httpError(409, "Email in use"));
+      // Перевірка на відмінності імені та оновлення
+      if (existingUser.name !== account.name) {
+        await db.query('UPDATE users SET name=$1 WHERE google_id=$2', [account.name, account.sub]);
       }
 
-    } else {
-
-      const idUser = uuidv4();
-      const password = await bcrypt.hash(uuidv4(), 10);
-
-      await db.query(`
-        INSERT INTO users (id, name, email, google_id, password, is_verified) 
-        VALUES ($1, $2, $3, $4, $5, true)
-        RETURNING id, name, email`,
-        [idUser, account.name, account.email, account.sub, password]
-      );
-
-      const user = {
-        id: idUser,
-        name: account.name,
-        email: account.email,
-        password,
-      };
-
-      done(null, user);
+      // Якщо користувач існує, повертаємо його
+      return done(null, existingUser);
     }
+
+    // Перевірка наявності користувача за email
+    const existingEmailQuery = await db.query('SELECT * FROM users WHERE email=$1', [account.email]);
+
+    if (existingEmailQuery.rows.length > 0) {
+      const existingUser = existingEmailQuery.rows[0];
+
+      // Перевірка на відмінності імені та оновлення
+      if (existingUser.name !== account.name) {
+        await db.query('UPDATE users SET name=$1 WHERE email=$2', [account.name, account.email]);
+      }
+
+      // Якщо користувач існує, повертаємо його
+      return done(null, existingUser);
+    }
+
+    // Якщо користувача не існує, створюємо його
+    const idUser = uuidv4();
+    const password = await bcrypt.hash(uuidv4(), 10);
+
+    await db.query(`
+      INSERT INTO users (id, name, email, google_id, password, is_verified) 
+      VALUES ($1, $2, $3, $4, $5, true)
+      RETURNING id, name, email`,
+      [idUser, account.name, account.email, account.sub, password]
+    );
+
+    const user = {
+      id: idUser,
+      name: account.name,
+      email: account.email,
+      password,
+    };
+
+    done(null, user);
   } catch (e) {
     done(e, false);
   }
