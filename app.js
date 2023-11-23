69030a1cb30be98931be5483e3e15b361d3e1409
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-11-23 13:59:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const logger = require('morgan')
 const cors = require("cors");
-// const passport = require("passport"); 
-// const session = require('express-session');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser')
-// require("./middlewares/auth/googleAuthenticate.js");
 
 dotenv.config({ path: './.env' }); 
 
@@ -20,37 +17,10 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 const { FRONT_PROD, FRONT_DEV } = process.env;
 
 app.use(logger(formatsLogger));
-// app.use(cors({
-//   origin: ['http://localhost:3000', 'https://nmarkhotsky.github.io/finance-front'],
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
-//   exposedHeaders: ['Content-Range', 'X-Content-Range'],
-// }));
-
-// app.use(cors({
-//   origin: ['https://nmarkhotsky.github.io/finance-front', 'http://localhost:3000'],
-//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-//   exposedHeaders: ['Access-Control-Allow-Origin',
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token',
-//     'X-Requested-With',
-//     'Accept',
-//     'Accept-Version',
-//     'Content-Length',
-//     'Content-MD5',
-//     'Content-Type',
-//     'Date',
-//     'X-Api-Version',
-//     'Authorization'],
-//   credentials: true,
-// }));
-
-// app.options('*', cors());
 
 const allowedOrigins = [
-  'https://nmarkhotsky.github.io',
-  'http://localhost:3000',
+  FRONT_PROD,
+  FRONT_DEV,
 ];
 
 const corsOptions = {
@@ -99,15 +69,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(cookieParser());
-
-// app.use(session({
-//   secret: process.env.COOKIE_SECRET,
-//   resave: false,
-//   saveUninitialized: true,
-// }))
-
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 app.use("/api", authRouter);
 app.use("/api/transactions", transactionsRouter);
