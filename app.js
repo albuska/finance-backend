4a4d9e0125957e3cef9235fdc8ab1cf53a4a1c55
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-11-24 23:56:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
   res.set('Access-Control-Allow-Credentials', true);
 
   // Дозволяємо доступ з усіх доменів (можна точніше налаштувати для конкретних)
-  res.set('Access-Control-Allow-Origin', allowedOrigins.join(', '));
+  res.set('Access-Control-Allow-Origin', allowedOrigins[0] || allowedOrigins[1]);
 
   next();
 });
